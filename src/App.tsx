645cce04d2f789513b40054c9463b14455d4999e
Caroline Vieira <caroline@fetch.ly>
2022-07-20 20:41:23 +0200

    Layout without tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from './components/Header';
+import { TaskBoard } from './components/TaskBoard';
 import './global.css';
 
 
 export function App() {
   return (
+    <>
     <Header />
+    <TaskBoard />
+    </>
   )
 }
 
--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,5 +1,45 @@
+import styles from './TaskBoard.module.css';
+import { ClipboardText, PlusCircle } from 'phosphor-react';
+import ClipBoardSvg from '../assets/clipboard.svg'
+
+
+
 export function TaskBoard(){
-    return(
-        <div>Task Board</div>
-    )
+	return(
+		<>
+		<form className={styles.todoForm}>
+			<textarea
+				placeholder='Add a new task'
+			/>
+			<button
+				type='submit'
+			>
+				Create
+				<PlusCircle weight='bold' size={18} />
+			</button>
+		</form>
+
+		<header className={styles.taskContainer}>
+			<div className={styles.taskCreated}>
+				<p>Tasks created</p>
+				<span>0</span>
+			</div>
+			<div className={styles.taskDone}>
+				<p>Tasks done</p>
+				<span>0</span>
+			</div>
+		</header>
+
+		<main className={styles.taskBox}>
+			<div className={styles.taskBoxContent}>
+				<img src={ClipBoardSvg} alt="Clipboard icon" />
+				<p>
+					You don't have any tasks registered yet.
+					Create tasks and organize your tasks
+				</p>
+			</div>
+
+		</main>
+		</>
+	)
 }
